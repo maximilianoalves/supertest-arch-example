33a654d32285dbe302266abb1083485a4d7088d4
Maximiliano <maximilianodacruz@gmail.com>
2021-07-27 11:14:44 -0300

    melhorias no código

--- test/usuarios/requests/usuarios.request.js
+++ test/usuarios/requests/usuarios.request.js
@@ -2,10 +2,6 @@
 const global = require('../../../utils/global')
 const supertest = require('supertest')
 
-const listar = {
-    get: supertest(global.base.url).get('usuarios').set('Accept', 'application/json')
-}
-
 module.exports = {
-    listar
+    get: supertest(global.base.url).get('usuarios').set('Accept', 'application/json')
 }
--- test/usuarios/tests/usuarios.test.js
+++ test/usuarios/tests/usuarios.test.js
@@ -5,15 +5,14 @@ const usuariosSchema = require('../contracts/usuarios.contract');
 
 describe('GET - Usuarios', () => {
     it('Deve buscar os usuários com sucesso', async () => {
-        usuarios.listar.get.expect(200).then(res => {
+        usuarios.get.expect(200).then(res => {
             chai.assert.isNotEmpty(res.body)
         })
     });
 
 
     it('Deve validar o contrato da lista de usuários', async () => {
-        let res = await usuarios.listar.get
-
+        let res = await usuarios.get
         joi.assert(res.body, usuariosSchema)
     });
 });
